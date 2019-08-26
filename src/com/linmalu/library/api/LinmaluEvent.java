4a9a8f116145514d47a959d2b3e2427f0d176cc7
Linmalu <gkdwns0103@naver.com>
2019-08-26 13:06:24 +0900

    1.13.2.1 메인 변수 추가 및 버전확인기능 추가

--- src/com/linmalu/library/api/LinmaluEvent.java
+++ src/com/linmalu/library/api/LinmaluEvent.java
@@ -1,11 +1,27 @@
 package com.linmalu.library.api;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public abstract class LinmaluEvent implements Listener
 {
+	protected final LinmaluMain _main;
+
 	public LinmaluEvent(LinmaluMain main)
 	{
+		_main = main;
 		main.getServer().getPluginManager().registerEvents(this, main);
+	}
+
+	@EventHandler
+	public void linmaluEvent(PlayerJoinEvent event)
+	{
+		Player player = event.getPlayer();
+		if(player.isOp())
+		{
+			LinmaluServer.version(_main, player);
+		}
 	}
 }
