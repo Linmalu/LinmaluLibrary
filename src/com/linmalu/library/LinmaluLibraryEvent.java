01143637626aa4553f7fd997bbb3886b6119566f
Linmalu <gkdwns0103@naver.com>
2019-08-26 23:51:04 +0900

    1.13.2.1 채널 기능 추가

--- src/com/linmalu/library/LinmaluLibraryEvent.java
+++ src/com/linmalu/library/LinmaluLibraryEvent.java
@@ -3,8 +3,16 @@ package com.linmalu.library;
 import com.linmalu.library.api.LinmaluBlacklist;
 import com.linmalu.library.api.LinmaluEvent;
 import com.linmalu.library.api.LinmaluMain;
+import com.linmalu.library.api.LinmaluPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRegisterChannelEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinmaluLibraryEvent extends LinmaluEvent
 {
@@ -16,6 +24,22 @@ public class LinmaluLibraryEvent extends LinmaluEvent
 	@EventHandler
 	public void event(PlayerJoinEvent event)
 	{
-		LinmaluBlacklist.check(event.getPlayer());
+		Player player = event.getPlayer();
+		LinmaluBlacklist.check(player);
+		player.setMetadata(LinmaluPlayer.LINMALU_CHANNEL, new FixedMetadataValue(_main, new ArrayList<String>()));
+	}
+
+	@EventHandler
+	@SuppressWarnings("unchecked")
+	public void event(PlayerRegisterChannelEvent event)
+	{
+		for(MetadataValue metadata : event.getPlayer().getMetadata(LinmaluPlayer.LINMALU_CHANNEL))
+		{
+			if(metadata.getOwningPlugin().toString().equals(_main.toString()))
+			{
+				((List<String>)metadata.value()).add(event.getChannel());
+			}
+		}
+
 	}
 }
