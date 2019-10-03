d4ea34e43d903d026d5959c55e9f6c251da2e3ec
Linmalu <gkdwns0103@naver.com>
2019-10-04 00:01:17 +0900

    1.13.2.2 이름 변경

--- src/com/linmalu/library/api/LinmaluLocation.java
+++ src/com/linmalu/library/api/LinmaluLocation.java
@@ -30,33 +30,33 @@ public class LinmaluLocation extends Location implements ConfigurationSerializab
 		super(world, x, y, z, (float)yaw, (float)pitch);
 	}
 
-	private static final String WORLD = "world";
-	private static final String X = "x";
-	private static final String Y = "y";
-	private static final String Z = "z";
-	private static final String YAW = "yaw";
-	private static final String PITCH = "pitch";
+	private static final String CONFIG_WORLD = "world";
+	private static final String CONFIG_X = "x";
+	private static final String CONFIG_Y = "y";
+	private static final String CONFIG_Z = "z";
+	private static final String CONFIG_YAW = "yaw";
+	private static final String CONFIG_PITCH = "pitch";
 
 	@Override
 	public Map<String, Object> serialize()
 	{
 		Map<String, Object> map = new LinkedHashMap<>();
-		map.put(WORLD, getWorld().getName());
-		map.put(X, getX());
-		map.put(Y, getY());
-		map.put(Z, getZ());
-		map.put(YAW, getYaw());
-		map.put(PITCH, getPitch());
+		map.put(CONFIG_WORLD, getWorld().getName());
+		map.put(CONFIG_X, getX());
+		map.put(CONFIG_Y, getY());
+		map.put(CONFIG_Z, getZ());
+		map.put(CONFIG_YAW, getYaw());
+		map.put(CONFIG_PITCH, getPitch());
 		return map;
 	}
 
 	public static LinmaluLocation deserialize(Map<String, Object> map)
 	{
-		World world = Bukkit.getWorld((String)map.get(WORLD));
+		World world = Bukkit.getWorld((String)map.get(CONFIG_WORLD));
 		if(world == null)
 		{
 			return null;
 		}
-		return new LinmaluLocation(world, (double)map.get(X), (double)map.get(Y), (double)map.get(Z), (double)map.get(YAW), (double)map.get(PITCH));
+		return new LinmaluLocation(world, (double)map.get(CONFIG_X), (double)map.get(CONFIG_Y), (double)map.get(CONFIG_Z), (double)map.get(CONFIG_YAW), (double)map.get(CONFIG_PITCH));
 	}
 }
